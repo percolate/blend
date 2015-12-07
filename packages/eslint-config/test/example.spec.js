a806be367aa9a7c0f48300d62548852c7ad29a08
nishp1 <nish@percolate.com>
2015-12-07 15:10:05 -0500

    prevent use of sinon fakes

--- packages/eslint-config/test/example.spec.js
+++ packages/eslint-config/test/example.spec.js
@@ -1,6 +1,19 @@
 import { expect } from 'chai'
+import sinon from 'sinon'
 
 describe('the thing', function () {
+    var sandbox
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create()
+        sandbox.spy()
+        sandbox.stub()
+        sandbox.mock()
+    })
+    afterEach(function () {
+        sandbox.resotre()
+    })
+
     it('should do stuff', function () {
         expect(2 * 2).to.equal(4)
         expect(true).to.be.true
