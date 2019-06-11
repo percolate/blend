06779dcf9ada13e76e2af07dfd4bfc2a8e2ffcee
Guillaume Mathieu <contact@gmathieu.com>
2019-06-11 15:35:42 -0700

    feat: add @percolate/prettier-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,6 @@ module.exports = {
         'no-extra-semi': 'off', // prettier conflict,
         'no-mixed-spaces-and-tabs': 'off', // prettier conflict,
 
-        'prettier/prettier': [
-            'error',
-            { printWidth: 110, tabWidth: 4, singleQuote: true, trailingComma: 'es5', semi: false },
-        ],
+        'prettier/prettier': ['error'],
     },
 }
