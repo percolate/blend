85a7b2f11bcaa62cf19e1bf65ae6d79a54dfbab3
Guillaume Mathieu <contact@gmathieu.com>
2019-06-27 20:12:43 -0700

    ci: configure @percolate/test-suite-name

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
     extends: ['plugin:@percolate/base', 'plugin:@percolate/node'],
+    rules: {
+        '@percolate/test-suite-name': ['error', { basePaths: [__dirname] }],
+    },
 }
