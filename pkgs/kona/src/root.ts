a441fdbf786dcc460374b137829a5dc0ab1eda82
Guillaume Mathieu <contact@gmathieu.com>
2019-07-08 14:04:27 -0700

    refactor: add ability to resolve multiple paths in root(…resolvePaths)

--- pkgs/kona/src/root.ts
+++ pkgs/kona/src/root.ts
@@ -4,11 +4,11 @@ import { resolve } from 'path'
 const findUp = require('find-up') // outdated @type/find-up
 
 let _root: string
-export function root(resolvePath?: string) {
+export function root(...resolvePaths: string[]) {
     if (!_root) {
         const gitDir = findUp.sync('.git', { type: 'directory' })
         if (!gitDir) throw new Error('Project must be in a git repo')
         _root = resolve(gitDir, '..')
     }
-    return resolvePath ? resolve(_root, resolvePath) : _root
+    return resolvePaths.length ? resolve(_root, ...resolvePaths) : _root
 }
