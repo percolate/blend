8af95cf92d4db00f1ed8e2014066c5309e9062f1
Guillaume Mathieu <contact@gmathieu.com>
2019-07-12 16:45:20 -0700

    fix: add more js/jsx extensions to default testMatch

--- pkgs/kona/src/jest.ts
+++ pkgs/kona/src/jest.ts
@@ -33,8 +33,8 @@ export const jest: Partial<Config.InitialOptions> = {
               ],
           ]
         : undefined,
-    testMatch: ['**/*.spec.ts'],
     restoreMocks: true,
+    testMatch: ['**/*.spec.{ts,tsx,js,jsx}'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/', '/tmp/'],
     watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 }
