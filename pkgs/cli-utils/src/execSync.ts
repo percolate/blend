e808ebe97b8d45a514c3388f577179ba6cf70889
Guillaume Mathieu <contact@gmathieu.com>
2019-08-27 10:31:06 -0700

    fix: verbose execSync duplicates error message

--- pkgs/cli-utils/src/execSync.ts
+++ pkgs/cli-utils/src/execSync.ts
@@ -2,27 +2,43 @@ import { forceExit } from './forceExit'
 import * as childProcess from 'child_process'
 import { color } from './color'
 
-export function execSync(
-    cmd: string,
-    opts: { cwd?: string; noStack?: boolean; verbose?: boolean; onError?(err: Error): void } = {}
-) {
-    const { cwd, noStack, onError, verbose } = opts
-    try {
-        if (verbose) {
-            console.log(color(cmd, 'grey'))
+interface IExecSyncBaseOpts {
+    cwd?: string
+    onError?(err: Error): void
+}
+
+interface IExecSyncStringOpts extends IExecSyncBaseOpts {
+    noStack?: boolean
+}
+
+interface IExecSyncVerboseOpts extends IExecSyncBaseOpts {
+    verbose: true
+}
+
+/* eslint-disable import/export */
+export function execSync(cmd: string, opts?: IExecSyncStringOpts): string
+export function execSync(cmd: string, opts?: IExecSyncVerboseOpts): void
+export function execSync(cmd: string, opts: IExecSyncStringOpts | IExecSyncVerboseOpts = {}) {
+    const { cwd, onError = forceExit } = opts
+
+    if ('verbose' in opts) {
+        console.log(color(cmd, 'grey'))
+        try {
             childProcess.execSync(cmd, { stdio: 'inherit', cwd })
-            return ''
+        } catch (e) {
+            // do not pass error to forceExit to avoid duplicating error message
+            onError(opts.onError ? e : undefined)
         }
+        return
+    }
+
+    try {
         return childProcess
             .execSync(cmd, { stdio: 'pipe', cwd })
             .toString()
             .trim()
     } catch (e) {
-        if (onError) {
-            onError(e)
-            return ''
-        } else {
-            return forceExit(noStack ? e.message : e)
-        }
+        onError(opts.noStack ? e.message : e)
+        return ''
     }
 }
