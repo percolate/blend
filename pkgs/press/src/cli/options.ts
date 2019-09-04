f6efb4a22a6c9cec05e63d6d5924057962629938
Guillaume Mathieu <contact@gmathieu.com>
2019-09-04 16:34:02 -0700

    fix: --repo default value

--- pkgs/press/src/cli/options.ts
+++ pkgs/press/src/cli/options.ts
@@ -1,18 +1,28 @@
-import { git, execSync } from '@percolate/cli-utils'
+import { git, execSync, forceExit } from '@percolate/cli-utils'
 
 export const BRANCH_OPT = {
-    default: process.env['CIRCLE_BRANCH'] || git.getCurrBranch(),
+    default: process.env.CIRCLE_BRANCH || git.getCurrBranch(),
     desc: 'Git branch',
     require: true,
     string: true,
 }
 export const HASH_OPT = {
-    default: process.env['CIRCLE_SHA1'] || git.getCurrHash(),
+    default: process.env.CIRCLE_SHA1 || git.getCurrHash(),
     desc: 'Git hash',
     require: true,
 }
 export const REPO_OPT = {
-    default: execSync('git rev-parse --show-toplevel').replace(/^\//, ''),
+    default: getRepoName(),
     desc: 'Repository name',
     require: true,
+}
+
+function getRepoName() {
+    if (process.env.CIRCLE_PROJECT_USERNAME && process.env.CIRCLE_PROJECT_REPONAME) {
+        return `${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}`
+    }
+    const url = execSync('git config --get remote.origin.url')
+    const match = url.match(/@github\.com[:/](.*)\.git$/)
+    if (!match) return forceExit(`Unable to determine repo from ${url}`)
+    return match[1]
 }
