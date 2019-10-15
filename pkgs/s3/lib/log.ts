16ebf2204dbdc2cadbf1920f2005dd9a21217292
Guillaume Mathieu <contact@gmathieu.com>
2019-10-14 17:02:55 -0700

    feat: convert to TS

--- pkgs/s3/lib/log.ts
+++
@@ -1,8 +0,0 @@
-exports.log = function log(message) {
-    process.stdout.write(`${message}\n`)
-}
-
-exports.forceExit = function forceExit(message) {
-    process.stderr.write(`${message}\n`)
-    process.exit(1)
-}
